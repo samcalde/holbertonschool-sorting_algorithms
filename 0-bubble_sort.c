7e6897bb59403a11301371ef12cd5350735802f7
Samuel <samuel@iMac-de-Andres.local>
2023-09-19 22:38:52 -0500

    task 0 test 1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned long int i, j;
+	int temp;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		for (j = 0; j < (size - 1 - i); j++)
+		{
+			if (array[j] > array [j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
